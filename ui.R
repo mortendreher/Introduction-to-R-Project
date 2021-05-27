813ca7e8af1ff552294b0652b1c6b59aa58e1775
Morten <dreher@mail.hs-ulm.de>
2021-05-27 16:33:27 +0200

    First draft of UI

---
+++ ui.R
@@ -0,0 +1,10 @@
+library(shiny)
+
+
+
+ui <-   navbarPage("Liverpool",
+                  tabPanel("Team members", "saasjajasjdajasdjadsj"),
+                  tabPanel("About liver cancer", "whoever"),
+                  tabPanel("Graphs"),
+                  tabPanel("Tables")
+                )
