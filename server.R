eee1feda79838db4709822e3698101f41d13b43a
Morten <dreher@mail.hs-ulm.de>
2021-06-28 02:17:57 +0200

    Added dataset tab, CI table somewhat working now

--- server.R
+++ server.R
@@ -4,8 +4,24 @@ library(ggplot2)
 library(survminer)
 library(gmodels)
 
-server = function(input, output){
+server = function(input, output, session){
   df <- read.csv("liver_cancer.csv", header=TRUE)
+  df <- rename(df, ID=X)
+  
+  dataset <- reactive({
+    select(df, c(ID, input$c_vars))
+  })
+  myChoices <- c('age','sex','height','weight','bmi','diet','smoker','cigs_per_day', 'packyears', 'alc', 'size', 'bili', 'hbv', 'hcv','dia', 'chol')
+  observe({
+    updateCheckboxGroupInput(session, 'c_vars', 
+                             choices=c('Sex (0=w, 1=m)'='sex', 'Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 
+                              'Diet'='diet','Cholesterol'='chol', 'Smoker'='smoker', 'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
+                              'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili', 'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia'),
+                             selected = if (input$c_all) myChoices)
+  })
+  output$out_dataset <- renderDataTable(
+    dataset()
+  )
   scatter <- reactive(
     if(input$scat_var_group == 'None'){
       ggplot(data=df, mapping=aes(x=as.numeric(unlist(df[colnames(df) == input$scat_var_x])), y=as.numeric(unlist(df[colnames(df) == input$scat_var_y])))) +
@@ -27,7 +43,7 @@ server = function(input, output){
   box <- reactive(
     if(input$box_var_group == 'None'){
       ggplot(data=df) + geom_boxplot(mapping = aes(x = as.numeric(unlist(df[colnames(df) == input$box_var_x])))) + 
-        labs(title=paste0("Variables ", colnames(df[colnames(df) == input$box_var_x])),
+        labs(title=paste0("Variable ", colnames(df[colnames(df) == input$box_var_x])),
              x=colnames(df[colnames(df) == input$box_var_x]))
     }else{
     ggplot(data=df) + geom_boxplot(mapping = aes(x = as.numeric(unlist(df[colnames(df) == input$box_var_x])), y=as.character(unlist(df[colnames(df) == input$box_var_group])),
@@ -57,23 +73,20 @@ server = function(input, output){
       select(df, input$tab_freq_1, input$tab_freq_2)
     }
     )
-  risk <- reactive({
-     select(df, input$tab_risk_1, input$tab_risk_2)
-
-     
-     
-     })
-  # ci <- reactive({
-  #   error <- qt(p=1-(input$slider_ci/2), df=length(as.numeric(unlist(df[colnames(df) == input$tab_ci])))-1)*
-  #     (sd(as.numeric(unlist(df[colnames(df) == input$tab_ci]))))/sqrt(length(as.numeric(unlist(df[colnames(df) == input$tab_ci]))))
-  #   
-  #   left <- mean(as.numeric(unlist(df[colnames(df) == input$tab_ci]))) - error
-  #   right <- mean(as.numeric(unlist(df[colnames(df) == input$tab_ci]))) + error
-  #   expr <- rbind(c("Alpha", "Lower limit", "Mean", "Upper limit", "Standard error"), 
-  #                 round(c(input$slider_ci, left, mean(as.numeric(unlist(df[colnames(df) == input$tab_ci]))), right, error),
-  #                       digits=2))
-  #   colnames = FALSE
-  # })
+  risk <- reactive(
+     if(input$tab_risk_1 != input$tab_risk_2) { 
+       select(df, input$tab_risk_1, input$tab_risk_2) 
+     }
+    else {
+      select(df, input$tab_risk_1)
+    }
+     )
+  ci_out <- reactive({
+    select(df, input$tab_ci)
+  })
+  get_alpha <- reactive(
+    input$slider_ci
+  )
   output$scatter <- renderPlot({
     scatter()
   })
@@ -100,27 +113,40 @@ server = function(input, output){
   })
   output$risk <- renderTable({
     risk_df <- risk()
-    
+    unique1 <- 0
+    unique2 <- 0
     if(length(risk_df) > 1) { 
       unique1 <- sort((unique(risk_df[,1])))
       unique2 <- sort((unique(risk_df[,2])))
-      m <- matrix(0,nrow=length(unique1), ncol=length(unique2)+1, dimnames=list(a=unique1, b=c("",unique2)))
-      m <- data.frame(m,row.names = unique1)
+      m <- matrix(0,nrow=length(unique1), ncol=length(unique2)+3)
+      m <- format.data.frame(data.frame(m,row.names = unique1),digits=0)
       m[,1] <- (unique1)
       for(i in (1:length(unique1))) {
-        
-        for(j in (2:(length(unique2)+1))) { 
-          
+        for(j in (2:(length(unique2)+1))) {
           m[i,j] <- (sum(risk_df[,1]==unique1[i] & risk_df[,2]==unique2[j-1]))
-          
           }
-        
       }
+      m[,4] <- as.numeric(m[,2])/(as.numeric(m[,2])+as.numeric(m[,3]))
+      m[,5] <- as.numeric(m[,2])/(as.numeric(m[,3]))
+      colnames(m) <- c(" ", unique2, "Risk", "Odds")
     }
-    colnames(m) <- c(" ", unique2)
-    format.data.frame(m, digits=0)
+    else {
+      m <- as.data.frame(table(risk()))
+      names(m) = c(input$tab_risk_1, "Freq")
+    }
+   
+    m
   })
-  # output$ci <- renderTable(
-  #   ci()
-  # )
+  output$ci <- renderTable({
+      ci_var <- as.numeric(unlist(ci_out()))
+      alpha <- get_alpha()
+      error <- qt(p=1-(alpha/2), df=length(ci_var)-1)*(sd(ci_var))/sqrt(length(ci_var))
+      left <- mean(ci_var) - error
+      right <- mean(ci_var) + error
+      tab <- rbind(c("Alpha", "Lower limit", "Mean", "Upper limit", "Standard error"),
+                  round(c(alpha, left, mean(ci_var), right, error),
+                        digits=4))
+      tab 
+      
+  })
 }
--- ui.R
+++ ui.R
@@ -37,16 +37,27 @@ ui <- navbarPage("Liver cancer",
                                )
                              )
                          ),
+                tabPanel("Dataset", fluidRow(h2("Dataset")), 
+                         column(2,checkboxInput(inputId='c_all', 'Select all',value = TRUE),
+                         checkboxGroupInput(inputId='c_vars', label="Select variables", 
+                                            choices = c('Sex (0=w, 1=m)'='sex', 'Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 
+                                                        'Diet'='diet','Cholesterol'='chol', 'Smoker'='smoker', 'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
+                                                        'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili', 'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia')
+                                            , selected = c('age','sex','height','weight','bmi','diet','smoker','cigs_per_day', 'packyears', 'alc', 'size', 'bili', 'hbv', 'hcv','dia', 'chol')),
+                         ),
+                         column(10,dataTableOutput(outputId='out_dataset'))
+                         
+                         ),
                 tabPanel("Graphs",
                          fluidRow(h2("Scatterplot"),
                            column(2, selectInput(inputId = 'scat_var_x', 'Choose variable for x', 
                                                  choices =
-                                                 c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi',
+                                                 c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
                                                   'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
                                                   'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'bili')),
                            column(2, selectInput(inputId = 'scat_var_y', 'Choose variable for y',
                                                  choices =
-                                                   c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi',
+                                                   c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
                                                      'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
                                                      'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'size')),
                            column(2, selectInput(inputId = 'scat_var_group', 'Choose grouping variable',
@@ -58,7 +69,7 @@ ui <- navbarPage("Liver cancer",
                          fluidRow(h2("Boxplot"),
                                   column(2, selectInput(inputId = 'box_var_x', 'Choose variable for x', 
                                                         choices =
-                                                          c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi',
+                                                          c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
                                                             'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
                                                             'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'bili')),
                                   column(2, selectInput(inputId = 'box_var_group', 'Choose grouping variable',
@@ -70,7 +81,7 @@ ui <- navbarPage("Liver cancer",
                          fluidRow(h2("Histogram"),
                                   column(2, selectInput(inputId = 'histo_var_x', 'Choose variable for x', 
                                                         choices =
-                                                          c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi',
+                                                          c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
                                                             'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
                                                             'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'bili')),
                                   column(6, offset = 4, plotOutput("histo"))
@@ -80,7 +91,7 @@ ui <- navbarPage("Liver cancer",
                          fluidRow(h2("Summaries"),
                                   column(2, selectInput(inputId = 'tab_sum', 'Choose a metric variable',
                                                         choices = 
-                                                          c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi',
+                                                          c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
                                                             'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
                                                             'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'bili')
                                                           ),
@@ -100,23 +111,24 @@ ui <- navbarPage("Liver cancer",
                          fluidRow(h2("Risks"),
                                   column(2, selectInput(inputId = 'tab_risk_1', ' Choose a categorical variable',
                                                         choices =
-                                                          c( 'Sex' = 'sex', 'Diet' = 'diet', 'Smoker' = 'smoker',
+                                                          c( 'Sex' = 'sex', 'Smoker' = 'smoker',
                                                              'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia'), selected = 'Sex')),
                                   column(2, selectInput(inputId = 'tab_risk_2', ' Choose a categorical variable',
                                                         choices =
-                                                          c( 'Sex' = 'sex', 'Diet' = 'diet', 'Smoker' = 'smoker',
-                                                             'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia'), selected = 'Diet')),
+                                                          c( 'Sex' = 'sex', 'Smoker' = 'smoker',
+                                                             'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia'), selected = 'hbv')),
                                   column(6, offset = 2, tableOutput("risk"))
-                                  )
-                         # fluidRow(h2("Confidence Intervalls"),
-                         #          column(2, selectInput(inputId = 'tab_ci', 'Choose a metric variable',
-                         #                                choices =
-                         #                                  c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi',
-                         #                                    'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
-                         #                                    'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'bili')
-                         #                                  ),
-                         #          column(2, sliderInput(inputId = 'slider_ci', ' Choose alpha',
-                         #                                value = 0.05, min = 0, max = 1, step = 0.005)),
-                         #          column(6, offset = 2, tableOutput("ci"))
+                                  ),
+                         fluidRow(h2("Confidence Intervalls"),
+                                  column(2, selectInput(inputId = 'tab_ci', 'Choose a metric variable',
+                                                        choices =
+                                                          c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
+                                                            'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
+                                                            'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'bili')
+                                                          ),
+                                  column(2, sliderInput(inputId = 'slider_ci', ' Choose alpha',
+                                                        value = 0.05, min = 0, max = 1, step = 0.005)),
+                                  column(6, offset = 2, tableOutput("ci"))
                          )
+)
 )
