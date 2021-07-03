5610fc0ca66bf1b96bf95e7163b6ce2394c547e3
Morten <dreher@mail.hs-ulm.de>
2021-07-03 12:44:33 +0200

    Graphs and Tables tabs now use sidebarLayout

--- ui.R
+++ ui.R
@@ -1,8 +1,6 @@
 library(shiny)
 library(knitr)
 library(shinythemes)
-library(plotly)
-source("global.R")
 
 rmdfiles <- c("about_liver.rmd")
 sapply(rmdfiles, knit, quiet = T)
@@ -15,63 +13,96 @@ ui <- navbarPage(theme = shinytheme("slate"),"Liver cancer",
                 tabPanel("Dataset",icon = icon("list-alt"), fluidRow(h2("Dataset")), 
                          column(2,checkboxInput(inputId='c_all', 'Select all',value = TRUE),
                          checkboxGroupInput(inputId='c_vars', label="Select variables", 
-                                            choices = c_vars_choices
-                                            , selected = c_vars_choices),
+                                            choices = c('Sex (0=w, 1=m)'='sex', 'Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 
+                                                        'Diet'='diet','Cholesterol'='chol', 'Smoker'='smoker', 'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
+                                                        'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili', 'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia')
+                                            , selected = c('age','sex','height','weight','bmi','diet','smoker','cigs_per_day', 'packyears', 'alc', 'size', 'bili', 'hbv', 'hcv','dia', 'chol')),
                          ),
                          column(10,dataTableOutput(outputId='out_dataset'))
                          
                          ),
                 tabPanel("Graphs",icon = icon("bar-chart-o"),
-                         fluidRow(h2("Scatterplot"),
-                           column(2, selectInput(inputId = 'scat_var_x', 'Choose variable for x', 
-                                                 choices = var_choices, selected = 'bili')),
-                           column(2, selectInput(inputId = 'scat_var_y', 'Choose variable for y',
-                                                 choices = var_choices, selected = 'size')),
-                           column(2, selectInput(inputId = 'scat_var_group', 'Choose grouping variable',
-                                                 choices = var_group_choices, selected = 'None')),
-                           column(6, plotlyOutput("scatter"))
+                         h2("Scatterplot"),
+                         sidebarLayout(
+                           sidebarPanel(selectInput(inputId = 'scat_var_x', 'Choose variable for x', 
+                                                 choices =
+                                                 c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
+                                                  'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
+                                                  'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'bili'),
+                                        (selectInput(inputId = 'scat_var_y', 'Choose variable for y',
+                                                 choices =
+                                                   c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
+                                                     'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
+                                                     'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'size')),
+                                        (selectInput(inputId = 'scat_var_group', 'Choose grouping variable',
+                                                 choices =
+                                                 c( 'None', 'Sex' = 'sex', 'Diet' = 'diet', 'Smoker' = 'smoker',
+                                                    'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia'), selected = 'None'))),
+                           mainPanel(plotOutput("scatter"))
                          ),
-                         fluidRow(h2("Boxplot"),
-                                  column(2, selectInput(inputId = 'box_var_x', 'Choose variable for x', 
-                                                        choices = var_choices, selected = 'bili')),
-                                  column(2, selectInput(inputId = 'box_var_group', 'Choose grouping variable',
-                                                        choices = var_group_choices, selected = 'None')),
-                                  column(6, offset = 2, plotOutput("box"))
+                         h2("Boxplot"),
+                         sidebarLayout(sidebarPanel(selectInput(inputId = 'box_var_x', 'Choose variable for x', 
+                                                        choices =
+                                                          c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
+                                                            'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
+                                                            'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'bili'),
+                                  (selectInput(inputId = 'box_var_group', 'Choose grouping variable',
+                                                        choices =
+                                                          c( 'None', 'Sex' = 'sex', 'Diet' = 'diet', 'Smoker' = 'smoker',
+                                                             'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia'), selected = 'None'))),
+                                  mainPanel(plotOutput("box"))
                          ),
-                         fluidRow(h2("Histogram"),
-                                  column(2, selectInput(inputId = 'histo_var_x', 'Choose variable for x', 
-                                                        choices = var_choices, selected = 'bili')),
-                                  column(6, offset = 4, plotOutput("histo"))
-                         ),
+                         h2("Histogram"),
+                         sidebarLayout(sidebarPanel(selectInput(inputId = 'histo_var_x', 'Choose variable for x', 
+                                                        choices =
+                                                          c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
+                                                            'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
+                                                            'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'bili')),
+                                  mainPanel(plotOutput("histo"))
+                         )
                 ),
                 tabPanel("Tables",icon = icon("table"),
-                         fluidRow(h2("Summaries"),
-                                  column(2, selectInput(inputId = 'tab_sum', 'Choose a metric variable',
-                                                        choices = var_choices, selected = 'bili')
+                         h2("Summaries"),
+                         sidebarLayout(sidebarPanel(selectInput(inputId = 'tab_sum', 'Choose a metric variable',
+                                                        choices = 
+                                                          c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
+                                                            'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
+                                                            'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'bili')
                                                           ),
-                                  column(6, offset = 4, tableOutput("sum"))
+                                  mainPanel(tableOutput("sum"))
                                   ),
-                         fluidRow(h2("Frequencies"),
-                                  column(2, selectInput(inputId = 'tab_freq_1', ' Choose a categorical variable',
-                                                        choices = var_group_choices, selected = 'Diet')),
-                                  column(2, selectInput(inputId = 'tab_freq_2', ' Choose a categorical variable',
-                                                        choices = var_group_choices, selected = 'Sex')),
-                                  column(6, offset = 2, tableOutput("freq"))
+                         h2("Frequencies"),
+                         sidebarLayout(sidebarPanel(selectInput(inputId = 'tab_freq_1', ' Choose a categorical variable',
+                                                        choices =
+                                                          c('Sex' = 'sex', 'Diet' = 'diet', 'Smoker' = 'smoker',
+                                                             'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia'), selected = 'Diet'),
+                                                    selectInput(inputId = 'tab_freq_2', ' Choose a categorical variable',
+                                                        choices =
+                                                          c( 'None', 'Sex' = 'sex', 'Diet' = 'diet', 'Smoker' = 'smoker',
+                                                             'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia'), selected = 'None')),
+                                  mainPanel(tableOutput("freq"))
                                   ),
-                         fluidRow(h2("Risks"),
-                                  column(2, selectInput(inputId = 'tab_risk_1', ' Choose a categorical variable',
-                                                        choices = var_risk_choices, selected = 'Sex')),
-                                  column(2, selectInput(inputId = 'tab_risk_2', ' Choose a categorical variable',
-                                                        choices = var_risk_choices, selected = 'hbv')),
-                                  column(6, offset = 2, tableOutput("risk"))
+                         h2("Risks"),
+                         sidebarLayout(sidebarPanel(selectInput(inputId = 'tab_risk_1', ' Choose a categorical variable',
+                                                        choices =
+                                                          c( 'Sex' = 'sex', 'Smoker' = 'smoker',
+                                                             'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia'), selected = 'Sex'),
+                                        selectInput(inputId = 'tab_risk_2', ' Choose a categorical variable',
+                                                        choices =
+                                                          c( 'Sex' = 'sex', 'Smoker' = 'smoker',
+                                                             'Hepatitis B' = 'hbv', 'Hepatitis C' = 'hcv', 'Diabetes' = 'dia'), selected = 'hbv')),
+                                  mainPanel(tableOutput("risk"))
                                   ),
-                         fluidRow(h2("Confidence Intervals"),
-                                  column(2, selectInput(inputId = 'tab_ci', 'Choose a metric variable',
-                                                        choices = var_choices, selected = 'bili')
-                                                          ),
-                                  column(2, sliderInput(inputId = 'slider_ci', ' Choose CI percentage',
+                         h2("Confidence Intervals"),
+                         sidebarLayout(sidebarPanel(selectInput(inputId = 'tab_ci', 'Choose a metric variable',
+                                                        choices =
+                                                          c('Age' = 'age','Height' = 'height', 'Weight' = 'weight', 'BMI' = 'bmi', 'Cholesterol'='chol',
+                                                            'Cigarettes per day' = 'cigs_per_day', 'Packyears' = 'packyears',
+                                                            'Alcohol (g/day)'= 'alc','Tumour size' = 'size', 'Bilirubin' = 'bili'), selected = 'bili')
+                                                          ,
+                                  sliderInput(inputId = 'slider_ci', ' Choose CI percentage',
                                                         value = 0.95, min = 0, max = 1, step = 0.01)),
-                                  column(6, offset = 2, tableOutput("ci"))
+                                  mainPanel(tableOutput("ci"))
                          )
 )
 )
